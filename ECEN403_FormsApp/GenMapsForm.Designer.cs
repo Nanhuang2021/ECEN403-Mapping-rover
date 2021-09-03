a723247610091e36320545a3c2babb8faf70a87e
Oscar-Rod-crypto <78818668+Oscar-Rod-crypto@users.noreply.github.com>
2021-09-03 15:46:56 -0500

    Add files via upload
    This directory holds all the components for the GUI application.

---
+++ ECEN403_FormsApp/GenMapsForm.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace ECEN403_FormsApp
+{
+    partial class GenMapsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::ECEN403_FormsApp.Properties.Resources.lidar_output_fixed;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(86, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(591, 362);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // GenMapsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "GenMapsForm";
+            this.Text = "Form5";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormClosure);
+            this.Load += new System.EventHandler(this.GenMapsForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ ECEN403_FormsApp/InputCommandsForm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ECEN403_FormsApp
+{
+    public partial class InputCommandsForm : Form
+    {
+        public InputCommandsForm(string FormName)
+        {
+            InitializeComponent();
+            this.Text = FormName;
+        }
+
+        private void Form4_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormClosure(object sender, FormClosingEventArgs e)
+        {
+            MainMenuForm parentForm = new MainMenuForm();
+
+            parentForm.Show();
+        }
+
+        private void EnterBtn_Click(object sender, EventArgs e)
+        {
+            // button will display commands on form in "sent commands"
+            // if user input textbox is empty, display an error form to prompt user
+            if (txtUserInput.Text == String.Empty) {
+                txtTxCommands.AppendText("ERROR: Input Commands must be filled.");
+            }
+            
+            // Can only tab, need to make new line
+            string displayInput = txtUserInput.Text;
+            txtTxCommands.AppendText(displayInput);
+            txtTxCommands.AppendText(Environment.NewLine);
+
+            // display sent commands that will be executed
+            // if a valid input is found...
+            if (txtUserInput.Text.ToLower() == "start" || txtUserInput.Text.ToLower() == "stop" || 
+                txtUserInput.Text.ToLower() == "map room" || txtUserInput.Text.ToLower() == "hello" || 
+                txtUserInput.Text.ToLower() == "roam")
+            {
+                // valid input, echo the input
+                txtRxCommands.AppendText(txtUserInput.Text);
+                txtRxCommands.AppendText(Environment.NewLine);
+            }
+
+            else
+            {
+                // invalid input
+                txtRxCommands.AppendText("Sorry, \"" + txtUserInput.Text + "\" is not a valid input");
+                txtRxCommands.AppendText(Environment.NewLine);
+            }
+        }
+    }
+}
